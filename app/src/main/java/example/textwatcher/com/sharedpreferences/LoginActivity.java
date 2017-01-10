83012dbd2b1aa3eff150151249d6abe7504f5ec4
Carlos Aguilera <ceaguileraa@tecmor.mx>
2017-01-09 18:58:38 -0600

    AlertDialog through ImageViewSettings in loginActivity added. view_settings.xml file created

--- app/src/main/java/example/textwatcher/com/sharedpreferences/LoginActivity.java
+++ app/src/main/java/example/textwatcher/com/sharedpreferences/LoginActivity.java
@@ -1,5 +1,6 @@
 package example.textwatcher.com.sharedpreferences;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
@@ -8,9 +9,11 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -19,28 +22,33 @@ public class LoginActivity extends AppCompatActivity {
 
     EditText user, password , etDis;
     Button loginButton;
+    ImageView imageButtonSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
         //Part A1. Understanding sharedPreferences
         etDis = (EditText)findViewById(R.id.etDis);
         Savesettings = getSharedPreferences("settingFile", MODE_PRIVATE);
-        // If value for key not exist then return second param value -
-        // ("isTv2Disabled",false) - in this case false
-        if (Savesettings.contains("isTv2Disabled") || Savesettings.getBoolean("isTv2Disabled",false)) {
-            etDis.setEnabled( false );
-            etDis.setVisibility(View.GONE);
-        }
 
         /* B1. Casting of the widgets*/
         user = (EditText) findViewById(R.id.user);
         password = (EditText) findViewById(R.id.password);
         loginButton = (Button) findViewById(R.id.button_login);
 
+        /*C1. Casting of the ImageView*/
+        imageButtonSettings = (ImageView)findViewById(R.id.button_settings);
+     /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+     /*++++++++++++++++++++++    END CASTING  ++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+        /* A2.  If value for key not exist then return second param value -
+            = if (a || b ) are false         ("isTv2Disabled",false) - in this case false */
+        if (Savesettings.contains("isTv2Disabled") || Savesettings.getBoolean("isTv2Disabled",false)) {
+            etDis.setEnabled( false );
+            etDis.setVisibility(View.GONE);
+        }
         /* B3. Set Text Watcher listener for the 2 editTexts*/
         user.addTextChangedListener(userAndPasswordFilledInWatcher);
         password.addTextChangedListener(userAndPasswordFilledInWatcher);
@@ -53,14 +61,28 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 preferences
                         .edit().putString(MainApplication.PREFERENCE_USER,user.getText().toString())
                         .apply();
-
                 login();
+            }
+        });
 
+        imageButtonSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               //Research pending when using a fragment format Layoutinflater inflater...... should be casted
+     /*public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);*/
+                LayoutInflater inflater = LayoutInflater.from(LoginActivity.this);
+                View dialogView = inflater.inflate(R.layout.view_settings, null);
 
+                new AlertDialog.Builder(LoginActivity.this)
+                        .setTitle("Android Logo")
+                        .setView(dialogView)
+                        .setPositiveButton("Accept",null) //Replace null for newDialogIterface.onClickListener....
+                        .setNegativeButton("Cancel", null)
+                        .show();
             }
         });
     }
