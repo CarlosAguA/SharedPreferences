70634b18bcb39fea1b85c00b418cddc5eb20c5be
Carlos Aguilera <ceaguileraa@tecmor.mx>
2017-01-10 16:58:40 -0600

    Image View for Android Icon was inserted with alertDialog succesfully working.

--- app/src/main/java/example/textwatcher/com/sharedpreferences/LoginActivity.java
+++ app/src/main/java/example/textwatcher/com/sharedpreferences/LoginActivity.java
@@ -1,6 +1,7 @@
 package example.textwatcher.com.sharedpreferences;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
@@ -14,6 +15,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -23,13 +26,16 @@ public class LoginActivity extends AppCompatActivity {
     EditText user, password , etDis;
     Button loginButton;
     ImageView imageButtonSettings;
+    int imageController = 0 ;
+    int droid = 0 ;
+    ImageView logo ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        //Part A1. Understanding sharedPreferences
+        /*Part A1. Understanding sharedPreferences specifying file name . S1. **/
         etDis = (EditText)findViewById(R.id.etDis);
         Savesettings = getSharedPreferences("settingFile", MODE_PRIVATE);
 
@@ -38,22 +44,29 @@ public class LoginActivity extends AppCompatActivity {
         password = (EditText) findViewById(R.id.password);
         loginButton = (Button) findViewById(R.id.button_login);
 
-        /*C1. Casting of the ImageView*/
+        /*C1. Casting of the Settings ImageView*/
         imageButtonSettings = (ImageView)findViewById(R.id.button_settings);
+
+        /*D1. Casting of the Android ImageView*/
+        logo = (ImageView)findViewById(R.id.androidLogo);
      /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
      /*++++++++++++++++++++++    END CASTING  ++++++++++++++++++++++++++++++++++++++++++++++++*/
 
-        /* A2.  If value for key not exist then return second param value -
-            = if (a || b ) are false         ("isTv2Disabled",false) - in this case false */
+        /* A2. Using a boolean for showing an editText in loginActivity
+          If value for key doesn´t exist, then return second param value -  if (a || b ) are false
+          @param Savesettings.getBoolean("isTv2Disabled",false) - in this case false */
         if (Savesettings.contains("isTv2Disabled") || Savesettings.getBoolean("isTv2Disabled",false)) {
             etDis.setEnabled( false );
             etDis.setVisibility(View.GONE);
         }
+
         /* B3. Set Text Watcher listener for the 2 editTexts*/
         user.addTextChangedListener(userAndPasswordFilledInWatcher);
         password.addTextChangedListener(userAndPasswordFilledInWatcher);
 
         //(getContext()
+        /* D. Using sharedPref within another class for managing the preference
+        * @param preferences.getString() */
         final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         user.setText(preferences.getString(MainApplication.PREFERENCE_USER, null));
 
@@ -68,24 +81,82 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        /*C2. Pop Up an AlertDialog after clicking the settings */
         imageButtonSettings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               //Research pending when using a fragment format Layoutinflater inflater...... should be casted
-     /*public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_login, container, false);*/
+               /*Note 1*/
                 LayoutInflater inflater = LayoutInflater.from(LoginActivity.this);
                 View dialogView = inflater.inflate(R.layout.view_settings, null);
 
-                new AlertDialog.Builder(LoginActivity.this)
-                        .setTitle("Android Logo")
-                        .setView(dialogView)
-                        .setPositiveButton("Accept",null) //Replace null for newDialogIterface.onClickListener....
-                        .setNegativeButton("Cancel", null)
-                        .show();
+               final RadioGroup androidLogo = (RadioGroup) dialogView.findViewById(R.id.androidLogo);
+
+               AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+              //RadioButton rb1 = (RadioButton) dialogView.findViewById(R.id.button1);
+
+                builder.setTitle("Android Logo");
+                builder.setView(dialogView);
+                builder.setPositiveButton("Ok",new DialogInterface.OnClickListener() { //Replace null for newDialogIterface.onClickListener....
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        SharedPreferences.Editor example2 = Savesettings.edit();
+                        // Show Pictures
+                        if (imageController == 1) {  example2.putInt("Droid", 1); example2.apply();
+                            logo.setImageResource(R.drawable.whitedroid);
+                            //logo.setVisibility(View.VISIBLE);
+                            }
+                        if (imageController == 2) {
+                            example2.putInt("Droid", 2);
+                            example2.apply();
+                            logo.setImageResource(R.drawable.blackdroid);
+                            // logo.setVisibility(View.VISIBLE);  }// Storing integer
+                        }
+                        if (imageController == 3) {
+                            example2.putInt("Droid", 3);
+                            example2.apply();
+                            logo.setImageResource(R.drawable.greendroid);
+                            //logo.setVisibility(View.VISIBLE); }
+                        }
+                            }
+                });
+                builder.show();
+                //addListenerRadioGroup();
+                androidLogo.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+                {
+                    public void onCheckedChanged(RadioGroup group, int checkedId) {
+                        switch(checkedId) {
+                            case R.id.button1: imageController = 1 ; Log.i("ID", "Boton1"); break;
+                            case R.id.button2: imageController = 2 ; Log.i("ID", "Boton2"); break;
+                            case R.id.button3: imageController = 3 ; Log.i("ID", "Boton3"); break;
+                        }
+                    }
+                });
             }
         });
+
+       // showAndroid();
     }
+
+    /*private void showAndroid() {
+
+       droid = Savesettings.getInt("whitedroid",0);
+        if (droid == 0) {
+            logo.setVisibility(View.GONE);
+        } else {
+            if (droid == 1) {
+                logo.setImageResource(R.drawable.whitedroid);
+                logo.setVisibility(View.VISIBLE);
+            }
+            if (droid == 2) {
+                logo.setImageResource(R.drawable.blackdroid);
+                logo.setVisibility(View.VISIBLE);
+            }
+            if (droid == 3) {
+                logo.setImageResource(R.drawable.greendroid);
+                logo.setVisibility(View.VISIBLE);
+            }
+        }
+    }*/
 
     private void login() {
         /* A - B */
@@ -98,7 +169,6 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-
 
     /* B2. This constructor will allow the tracking when the userField & passwordField aren´t
     empty and then enabling the login button */
@@ -122,3 +192,10 @@ public class LoginActivity extends AppCompatActivity {
         }
     };
 }
+
+
+/* Note 1 : //Research pending when using a fragment format Layoutinflater inflater...... should be casted
+     /*public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);*/
+
+/*S1 - http://stackoverflow.com/questions/2614719/how-do-i-get-the-sharedpreferences-from-a-preferenceactivity-in-android*/
